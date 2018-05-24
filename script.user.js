d7b353cebed0f790cce64cca603f8cbea7f6fdc9
XiongQi <xiongqi.me@foxmail.com>
2018-05-24 23:28:53 +0800

    feat: 战旗界面改进

--- script.user.js
+++ script.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         直播界面精简
 // @namespace    xiongqi
-// @version      1.4
+// @version      1.5
 // @description  斗鱼,战旗直播界面精简,持续更新中
 // @author       XiongQi
 // @match        https://www.douyu.com/*
@@ -41,6 +41,10 @@
       mainbody.style.margin = '0';
       mainbody.style.padding = '10px 150px';
       mainbody.appendChild(video);
+      setTimeout(() => {
+        const bottom = document.getElementById('js-stats-and-actions');
+        bottom.removeChild(document.getElementsByClassName('task-Getyw')[0]);
+      }, 10000);
     },
   };
   const zhanqiFunc = {
@@ -48,25 +52,44 @@
       clearDOM('js-room-super-panel');
     },
     videoUI() {
-      // const room = document.getElementById('js-room-super-panel');
-      const style = document.createElement('style');
-      style.innerHTML =
-        '.theatre .live-star-page .live-room-content .video-flash-cont{width: 100% !important;height:100% !important;}';
-      document.body.appendChild(style);
-      const bottom = document.getElementsByClassName('room-star-bottom-skin')[0];
-      const bottom_ad = document.getElementsByClassName('live-stars-bottom')[0];
-      bottom.removeChild(bottom_ad);
-      const content = document.getElementsByClassName('live-star-content')[0];
-      const chat = document.getElementById('js-right-chat-panel');
-      content.removeChild(chat);
-      const content_left_bottom = document.getElementsByClassName('left-area')[0];
-      const content_left_bottom_ad = document.getElementsByClassName('left-bottom-area')[0];
-      content_left_bottom.removeChild(content_left_bottom_ad);
-      const video = document.getElementsByClassName('live-room-content')[0];
-      video.style.width = '100%';
-      const gift = document.getElementsByClassName('liveMessage')[0];
-      gift.style.position = 'relative';
-      gift.style.zIndex = 'unset';
+      const panel = document.getElementById('js-room-super-panel');
+      if (panel.classList.contains('live-star-page')) {
+        // const room = document.getElementById('js-room-super-panel');
+        const style = document.createElement('style');
+        style.innerHTML =
+          '.theatre .live-star-page .live-room-content .video-flash-cont{width: 100% !important;height:100% !important;} .liveMessage{z-index:unset !important}';
+        document.body.appendChild(style);
+        const bottom = document.getElementsByClassName('room-star-bottom-skin')[0];
+        const bottom_ad = document.getElementsByClassName('live-stars-bottom')[0];
+        const top = document.getElementsByClassName('live-stars-top-content')[0];
+        bottom.removeChild(bottom_ad);
+        top.removeChild(document.getElementById('js-room-desc-collapse-btn'));
+        const content = document.getElementsByClassName('live-star-content')[0];
+        const chat = document.getElementById('js-right-chat-panel');
+        content.removeChild(chat);
+        // const content_left_bottom = document.getElementsByClassName('left-area')[0];
+        // const content_left_bottom_ad = document.getElementsByClassName('left-bottom-area')[0];
+        // content_left_bottom.removeChild(content_left_bottom_ad);
+        const video = document.getElementsByClassName('live-room-content')[0];
+        video.style.width = '100%';
+        // const gift = document.getElementsByClassName('liveMessage')[0];
+        // gift.style.position = 'relative';
+        // gift.style.zIndex = 'unset';}
+      } else {
+        const right = document.getElementById('js-right-chat-panel');
+        panel.removeChild(right)
+        panel.removeChild(document.getElementById('js-right-chat-show-btn'))
+        const content = document.getElementById('js-pull-ads-layer');
+        const content_bottom = document.getElementById('js-room-module-area');
+        content.style.paddingRight='50px'
+        content.style.paddingLeft='100px'
+        content.removeChild(content_bottom)
+        content.removeChild(document.getElementsByClassName('live-gg-area')[0])
+        const style = document.createElement('style');
+        style.innerHTML =
+          '.theatre .live-room-content .video-flash-cont{width: 100% !important;height:100% !important;} .liveMessage{z-index:unset !important} .live-room-content{padding: 0 80px 0 140px;}';
+        document.body.appendChild(style);
+      }
     },
   };
   window.onload = function() {
